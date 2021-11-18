2bb443ad7c3b789f219dfe13a7ee74cef3e80761
Sanjith <85774398+Sanjith7146@users.noreply.github.com>
2021-11-18 15:00:01 +0800

    Initial Commit

---
+++ Time Planner/Time_PlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_PlannerApp.swift
+//  Time Planner
+//
+//  Created by sanjithsethu on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Time_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

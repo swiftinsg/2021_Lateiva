73511245408aba047b627b4c7a853ce8c85455b2
Sanjith <85774398+Sanjith7146@users.noreply.github.com>
2021-11-26 14:42:06 +0800

    Added the App Icons

---
+++ Time Planner/ActivitySetsData.swift
@@ -0,0 +1,8 @@
+//
+//  ActivitySetsData.swift
+//  Time Planner
+//
+//  Created by sanjithsethu on 26/11/21.
+//
+
+import Foundation
---
+++ Time Planner/EventsData.swift
@@ -0,0 +1,8 @@
+//
+//  EventsData.swift
+//  Time Planner
+//
+//  Created by sanjithsethu on 26/11/21.
+//
+
+import Foundation
